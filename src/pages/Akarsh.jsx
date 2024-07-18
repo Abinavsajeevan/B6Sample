c5f528af6da57b4ea9b9b430e2268c402afe20d9
Abinavsajeevan <abinavabi2k02@gmail.com>
2024-07-18 21:46:30 +0530

    changes of akarsh code

--- src/pages/Akarsh.jsx
+++ src/pages/Akarsh.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 function Akarsh() {
   return (
     <>
-      <h1 className='text-center text-warning mt-5'> Akarsh A</h1>
-      <p className='fs-1 fw-bold text-center mt-5'>I am from <span className='text-danger'>Kannur</span> 💣💣</p>
+      <div style={{height:'60vh'}} className='bg-white-50 text-center'>
+        <h1 className='text-center text-warning mt-5'> Akarsh A</h1>
+        <p className='fs-1 fw-bold text-center mt-5'>I am from <span className='text-danger'>Kannur</span> 💣💣</p>
+      </div>
     </>
   )
 }
